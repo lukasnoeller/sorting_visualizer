8520f83da85fe45fde88db1d7fe4f00c7086c5cd
Lukas <lukasnoller@gmail.com>
2023-10-15 19:33:41 +0200

    Implement visualization

--- visualizer.cpp
+++ visualizer.cpp
@@ -1,17 +1,49 @@
+
+#include <iostream>
+#include "raylib.h"
+using namespace std;
+#define MOUSE_SCALE_MARK_SIZE   12
 /*******************************************************************************************
 *Sorting algorithm visualizer
 *  
 *
 ********************************************************************************************/
 
-#include <iostream>
-#include "raylib.h"
-using namespace std;
-#define MOUSE_SCALE_MARK_SIZE   12
 
 // quicksort function with pivot point at end of array
 void quicksort_lomuto(int* array, int size_array, int low, int high)
 { 
+    Music music = LoadMusicStream("resources/ff7_enemy_death.mp3");
+
+    PlayMusicStream(music);
+    const int screenWidth = 1900;
+    const int screenHeight = 950;
+    const int margin_vert = int(0.05 * screenHeight);
+    const int margin_hori_want = int(0.015* screenWidth);
+    
+    int width = int((screenWidth - 2 * margin_hori_want)/size_array);
+    int height;
+    int margin_hori = int((screenWidth - size_array * width) / 2);
+    int x_pos = margin_hori;
+    Rectangle recs[size_array];
+    for(int i = 0; i<size_array; i++)
+    {
+        height = array[i] % (screenHeight - margin_vert); 
+        x_pos = x_pos + width;
+        Rectangle rec = { x_pos, screenHeight - height - margin_vert, width, height };
+        recs[i] = rec;
+    }
+
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+               for(int i = 0; i<sizeof(recs) / sizeof(Rectangle); i++)
+            {
+                DrawRectangleLinesEx(recs[i], 1, RED);
+                
+            }
+        EndDrawing();
+   
+
     cout<<"low:"<< low<< "\n";
     cout<<"high:"<< high<< "\n";
     int array_lower[size_array];
@@ -83,8 +115,9 @@ void quicksort_lomuto(int* array, int size_array, int low, int high)
             return;
         }
         cout<<"doing lower half \n";
+        UnloadMusicStream(music);   // Unload music stream buffers from RAM
         quicksort_lomuto(array,size_array, low, low + size_lower-1);
-        cout<<"doing uppeer half \n";
+        cout<<"doing upper half \n";
         quicksort_lomuto(array, size_array, low + size_lower,high);
         return;
     }
@@ -96,48 +129,54 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 1800;
+    int num_recs = 1000;
+    int test_array[num_recs];
+    Rectangle recs[num_recs];
+
+    const int screenWidth = 1900;
     const int screenHeight = 950;
     const int margin_vert = int(0.05 * screenHeight);
-    const int margin_hori_want = int(0.02* screenWidth);
-    int num_recs = 10;
+    const int margin_hori_want = int(0.015* screenWidth);
+    
     int width = int((screenWidth - 2 * margin_hori_want)/num_recs);
     int height;
     int margin_hori = int((screenWidth - num_recs * width) / 2);
     int x_pos = margin_hori;
-    Rectangle recs[num_recs];
+    
    
-    //int size_array = sizeof(test_array) / sizeof(int);
-    int size_array = 100;
-    int test_array[size_array];
+    InitWindow(screenWidth, screenHeight, "Sorting algorithm visualizer");
+    InitAudioDevice();              // Initialize audio device
 
-    for(int i = 0; i<size_array; i++)
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    for(int i = 0; i<num_recs; i++)
         {
-            test_array[i] = rand()% 100;
+            test_array[i] = rand()% 900;
             cout << "test_array[" << i <<"]:" << test_array[i]<< "\n" ;
         
         }
-    quicksort_lomuto(test_array, size_array, 0, size_array -1 );
-    for(int i = 0; i<size_array; i++)
+    quicksort_lomuto(test_array, num_recs, 0, num_recs -1 );
+    for(int i = 0; i<num_recs; i++)
     {
         cout << "sorted_array[" << i <<"]:" << test_array[i]<< "\n" ;
     
     }
-    for(int i = 0; i<sizeof(recs) / sizeof(Rectangle); i++)
-    {
-        height = rand() % (screenHeight - margin_vert); 
-        x_pos = x_pos + width;
-        Rectangle rec = { x_pos, screenHeight - height - margin_vert, width, height };
-        recs[i] = rec;
-        cout << "rec[" << i <<"].x:" << rec.x<< "\n" ;
-        cout << "rec[" << i <<"].y:" << rec.y<< "\n" ;
-        cout << "rec[" << i <<"].width:" << rec.width<< "\n" ;
-        cout << "rec[" << i <<"].height:" << rec.height<< "\n" ;
-    }
-    InitWindow(screenWidth, screenHeight, "Sorting algorithm visualizer");
+    // for(int i = 0; i<sizeof(recs) / sizeof(Rectangle); i++)
+    // {
+    //     height = test_array[i] % (screenHeight - margin_vert); 
+    //     x_pos = x_pos + width;
+    //     Rectangle rec = { x_pos, screenHeight - height - margin_vert, width, height };
+    //     recs[i] = rec;
+    //     cout << "rec[" << i <<"].x:" << rec.x<< "\n" ;
+    //     cout << "rec[" << i <<"].y:" << rec.y<< "\n" ;
+    //     cout << "rec[" << i <<"].width:" << rec.width<< "\n" ;
+    //     cout << "rec[" << i <<"].height:" << rec.height<< "\n" ;
+    // }
+   
+    
+    CloseAudioDevice();         // Close audio device (music streaming is automatically stopped)
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
@@ -151,12 +190,12 @@ int main(void)
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-            ClearBackground(RAYWHITE);
+       //     ClearBackground(RAYWHITE);
 
             DrawText("Visualize the array elements", 10, 10, 20, GRAY);
             for(int i = 0; i<sizeof(recs) / sizeof(Rectangle); i++)
             {
-                DrawRectangleLinesEx(recs[i], 1, RED);
+                //DrawRectangleLinesEx(recs[i], 1, RED);
                 
             }
 
